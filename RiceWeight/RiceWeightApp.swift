c6ef4b743f8cfba418f4137294a8f4e7771a0fff
RiceMarch <ricemarch@foxmail.com>
2026-05-31 19:56:07 +0800

    Initial Commit

---
+++ RiceWeight/RiceWeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiceWeightApp.swift
+//  RiceWeight
+//
+//  Created by 谭文韬 on 2026/5/31.
+//
+
+import SwiftUI
+
+@main
+struct RiceWeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
